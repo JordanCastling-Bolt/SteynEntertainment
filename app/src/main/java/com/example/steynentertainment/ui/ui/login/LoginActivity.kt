2b2e7e3f1f5658d1a1aabc15d35ba19025efb384
Jordan Castling-Bolt <jordancastlingbolt@gmail.com>
2023-09-08 08:33:58 +0200

    google sign in function

--- app/src/main/java/com/example/steynentertainment/ui/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/steynentertainment/ui/ui/login/LoginActivity.kt
@@ -17,11 +17,19 @@ import com.example.steynentertainment.databinding.ActivityLoginBinding
 import android.content.Intent
 import com.example.steynentertainment.ui.forgot_password.ForgotPasswordActivity
 import com.example.steynentertainment.ui.register.RegisterActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var loginViewModel: LoginViewModel
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,11 +37,24 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+
         val username = binding.username
         val password = binding.password
         val login = binding.login
         val loading = binding.loading
         val signUp = binding.signUp
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.web_client_id))  // Replace with your web client ID
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        binding.googleFab?.setOnClickListener {
+            signIn(googleSignInClient)
+        }
 
         binding.forgotPassword?.setOnClickListener {
             val intent = Intent(this@LoginActivity, ForgotPasswordActivity::class.java)
@@ -111,6 +132,43 @@ class LoginActivity : AppCompatActivity() {
                 loginViewModel.login(username.text.toString(), password.text.toString())
             }
         }
+    }
+    private fun signIn(googleSignInClient: GoogleSignInClient) {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                // Google Sign-In failed
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Successfully signed in
+                    val user = auth.currentUser
+                    updateUiWithUser(LoggedInUserView(user!!.displayName!!))
+                } else {
+                    // Sign-in failed
+                    showLoginFailed(R.string.login_failed)
+                }
+            }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
     }
 
     private fun updateUiWithUser(model: LoggedInUserView) {
