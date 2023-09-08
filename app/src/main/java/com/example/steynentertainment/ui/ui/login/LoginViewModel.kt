36227c1b17f7979bc001037b0f784fec2f1902bc
Jordan Castling-Bolt <jordancastlingbolt@gmail.com>
2023-09-08 09:57:03 +0200

    update

--- app/src/main/java/com/example/steynentertainment/ui/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/steynentertainment/ui/ui/login/LoginViewModel.kt
@@ -2,20 +2,14 @@ package com.example.steynentertainment.ui.ui.login
 
 import android.content.Context
 import android.os.Build
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import android.util.Patterns
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.example.steynentertainment.R
-import com.example.steynentertainment.ui.data.KeystoreHelper
 import com.example.steynentertainment.ui.data.LoginRepository
 import com.example.steynentertainment.ui.data.Result
-import com.example.steynentertainment.ui.data.SharedPreferencesHelper
-import com.example.steynentertainment.ui.data.model.LoggedInUser
-import java.security.SecureRandom
 
 
 class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
@@ -33,38 +27,9 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
         if (result is Result.Success) {
             _loginResult.value = LoginResult(success = LoggedInUserView(displayName = result.data.displayName))
 
-            // Generate your session token here
-            val sessionToken = generateSessionToken(result.data) // Implement your token generation logic
-            // Log to check if the session token is being generated
-            Log.d("TokenCheck", "Session token is generated: $sessionToken")
-            // Encrypt the token using Keystore
-            KeystoreHelper.generateSecretKey()
-            val encryptedToken = KeystoreHelper.encrypt(sessionToken)
-
-            // Save this encrypted token to SharedPreferences
-            SharedPreferencesHelper.saveToken(context, encryptedToken)
-
-
         } else {
             _loginResult.value = LoginResult(error = R.string.login_failed)
         }
-    }
-
-
-    // A dummy function to generate a session token (You should replace this with a real implementation)
-    private fun generateSessionToken(data: LoggedInUser): String {
-        // Generate a random 64-byte (512-bit) value
-        val random = SecureRandom()
-        val bytes = ByteArray(64)
-        random.nextBytes(bytes)
-
-        // Convert the random bytes to a hexadecimal string
-        val token = bytes.joinToString("") { "%02x".format(it) }
-
-        // Add some user-specific information (e.g., user ID) and timestamp to the token
-        val timestamp = System.currentTimeMillis()
-        Log.d("TokenGeneration", "Generated Token: $token")
-        return "$token-UID${data.userId}-TS$timestamp"
     }
 
 
